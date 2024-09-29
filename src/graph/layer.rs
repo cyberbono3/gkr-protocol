441b389c8a00dcfc0c6553e9d344b941b3ff53a2
Andrei Ivasko <andrei@fantom.foundation>
2024-09-29 20:42:19 +0300

    WIP

--- src/graph/layer.rs
+++ src/graph/layer.rs
@@ -1,27 +1,138 @@
-use crate::poly::MultiPoly;
+use crate::poly::*;
+use ark_bn254::Fr as ScalarField;
 
+// #[derive(Debug, PartialEq, Clone)]
+// struct BaseLayer {
+//     k: usize,
+//     prev_k: usize,
+//     add: MultiPoly,
+//     mult: MultiPoly,
+//     w_b: MultiPoly,
+//     w_c: MultiPoly,
+// }
 
-#[derive(Debug, PartialEq, Clone)]
-pub struct BaseLayer {
-    k: usize,
-    prev_k: usize,
-    add: MultiPoly,
-    mult: MultiPoly,
-    w_b: MultiPoly,
-    w_c: MultiPoly,
-}
+// impl BaseLayer {
+//     pub fn new(k: usize, prev_k: usize, add: MultiPoly,  mult: MultiPoly,  w_b: MultiPoly, w_c: MultiPoly) -> Self {
+//         Self {
+//             k,
+//             prev_k,
+//             add,
+//             mult, 
+//             w_b,
+//             w_c
+//         }
+//     }
+// }
 
 #[derive(Debug, PartialEq, Clone)]
 pub enum Layer {
     OutputLayer {
-        base_layer: BaseLayer,
+        k: usize,
+        prev_k: usize,
+    add: MultiPoly,
+    mult: MultiPoly,
+    w_b: MultiPoly,
+    w_c: MultiPoly,
         d: MultiPoly,
     },
     InterLayer {
-        base_layer: BaseLayer,
+        k: usize,
+        prev_k: usize,
+        add: MultiPoly,
+        mult: MultiPoly,
+        w_b: MultiPoly,
+        w_c: MultiPoly,
     },
     InputLayer {
         k: usize,
         input_ext: MultiPoly,
     },
+}
+
+impl Layer {
+    pub fn evaluation_ext(&self) -> MultiPoly {
+        match self {
+            Self::InputLayer { k: _, input_ext } => input_ext.clone(),
+            Self::OutputLayer { d, .. } => d.clone(),
+            Self::InterLayer { .. } => panic!(),
+        }
+    }
+
+
+    pub fn w_ext_gate_eval(&self, r: &Vec<ScalarField>) -> MultiPoly {
+        match self {
+            Self::InputLayer { k: _, input_ext } => input_ext.clone(),
+            Self::InterLayer {
+                add,
+                mult,
+                w_b,
+                w_c,
+                ..
+            }
+            | Self::OutputLayer {
+                add,
+                mult,
+                w_b,
+                w_c,
+                ..
+            } => {
+                let mut reduced_add_poly = mult_poly(&evaluate_variable(add, r), &(w_b + w_c));
+                reduced_add_poly = neg_shift_poly_by_k(&reduced_add_poly, r.len());
+                let mut reduced_mult_poly =
+                    mult_poly(&evaluate_variable(mult, r), &mult_poly(&w_b, &w_c));
+                reduced_mult_poly = neg_shift_poly_by_k(&reduced_mult_poly, r.len());
+                reduced_add_poly + reduced_mult_poly
+            }
+        }
+    }
+
+    
+
+    pub fn w_ext(&self) -> MultiPoly {
+        match self {
+            Self::InputLayer { k: _, input_ext } => input_ext.clone(),
+            Self::InterLayer {
+                k: _,
+                prev_k,
+                add,
+                mult,
+                w_b,
+                w_c,
+            }
+            | Self::OutputLayer {
+                k: _,
+                prev_k,
+                add,
+                mult,
+                w_b,
+                w_c,
+                ..
+            } => {
+                let f = mult_poly(add, &(w_b + w_c)) + mult_poly(mult, &mult_poly(&w_b, &w_c));
+                sum_last_k_var(&f, 2 * prev_k)
+            }
+        }
+    }
+
+    pub fn w_ext_line_restricted_values(
+        &self,
+        r: &Vec<ScalarField>,
+        q0: ScalarField,
+        q1: ScalarField,
+    ) -> ScalarField {
+        match self {
+            Self::InputLayer { .. } => panic!(),
+            Self::InterLayer { add, mult, .. } | Self::OutputLayer { add, mult, .. } => {
+                add.evaluate(r) * (q0 + q1) + mult.evaluate(r) * (q0 * q1)
+            }
+        }
+    }
+
+    pub fn k(&self) -> usize {
+        match self {
+            Self::InputLayer { k, .. }
+            | Self::InterLayer { k, .. }
+            | Self::OutputLayer { k, .. } => *k,
+        }
+    }
 }
--- src/poly.rs
+++ src/poly.rs
@@ -1,4 +1,88 @@
+use std::cmp::max;
+
+use ark_ff::Zero;
+
+use ark_poly::DenseMVPolynomial;
 use ark_bn254::Fr as ScalarField;
 use ark_poly::polynomial::multivariate::{SparsePolynomial, SparseTerm, Term};
 
+
 pub type MultiPoly = SparsePolynomial<ScalarField, SparseTerm>;
+
+
+
+pub fn mult_poly(p1: &MultiPoly, p2: &MultiPoly) -> MultiPoly {
+    let p1_terms = p1.terms();
+    let p2_terms = p2.terms();
+    let num_vars = max(p1.num_vars(), p2.num_vars());
+    let mut mult_terms = vec![];
+    for (unit_1, term_1) in p1_terms {
+        for (unit_2, term_2) in p2_terms {
+            let mut mult_term: Vec<_> = (*term_1).to_vec();
+            mult_term.append(&mut term_2.to_vec());
+            mult_terms.push((unit_1 * unit_2, SparseTerm::new(mult_term)));
+        }
+    }
+    MultiPoly::from_coefficients_vec(num_vars, mult_terms)
+}
+
+pub fn neg_shift_poly_by_k(p: &MultiPoly, k: usize) -> MultiPoly {
+    let terms = p.terms();
+    let current_num_vars = p.num_vars();
+    let mut shifted_terms = vec![];
+    for (unit, term) in terms {
+        let shifted_term = SparseTerm::new((*term).iter().map(|c| (c.0 - k, c.1)).collect());
+        shifted_terms.push((*unit, shifted_term));
+    }
+    MultiPoly::from_coefficients_vec(current_num_vars - k, shifted_terms)
+}
+
+pub fn evaluate_variable(p: &MultiPoly, r: &Vec<ScalarField>) -> MultiPoly {
+    if p.is_zero() {
+        return p.clone();
+    }
+    let mut new_coefficients = vec![];
+    let new_num_vars = p.num_vars();
+    for (unit, term) in p.terms() {
+        let mut new_unit = *unit;
+        let mut new_term = vec![];
+        for (var, power) in (*term).iter() {
+            if var < &r.len() {
+                for _ in 0..*power {
+                    new_unit = new_unit * r[*var];
+                }
+            } else {
+                new_term.push((*var, *power));
+            }
+        }
+        new_coefficients.push((new_unit, SparseTerm::new(new_term)));
+    }
+    MultiPoly::from_coefficients_vec(new_num_vars, new_coefficients)
+}
+
+pub fn sum_last_k_var(p: &MultiPoly, k: usize) -> MultiPoly {
+    if p.is_zero() {
+        return p.clone();
+    }
+    let mut new_coefficients = vec![];
+    let new_num_vars = p.num_vars() - k;
+    for (unit, term) in p.terms() {
+        let mut new_term = vec![];
+        let mut num_reduced_terms = 0;
+        for (var, power) in (*term).iter() {
+            if (p.num_vars() - var) <= k {
+                num_reduced_terms += 1;
+            } else {
+                new_term.push((*var, *power));
+            }
+        }
+        let mut new_unit = *unit;
+        for _ in 0..2_i32.pow((k - num_reduced_terms) as u32) {
+            new_unit += unit;
+        }
+        new_coefficients.push((new_unit, SparseTerm::new(new_term)));
+    }
+    MultiPoly::from_coefficients_vec(new_num_vars, new_coefficients)
+}
+
+
