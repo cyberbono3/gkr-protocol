b48472b52eac98fde389c4e50d765665f7b8ca5f
Andrei Ivasko <andrei@fantom.foundation>
2024-10-11 17:57:41 +0300

    wip layer tests

--- src/graph/layer.rs
+++ src/graph/layer.rs
@@ -52,6 +52,52 @@ pub enum Layer {
 }
 
 impl Layer {
+    pub fn new_output_layer(
+        k: usize,
+        prev_k: usize,
+        add: MultiPoly,
+        mult: MultiPoly,
+        w_b: MultiPoly,
+        w_c: MultiPoly,
+        d: MultiPoly,
+    ) -> Self {
+        Self::OutputLayer {
+            k,
+            prev_k,
+            add,
+            mult,
+            w_b,
+            w_c,
+            d,
+        }
+    }
+
+    // Constructor for InterLayer
+    pub fn new_inter_layer(
+        k: usize,
+        prev_k: usize,
+        add: MultiPoly,
+        mult: MultiPoly,
+        w_b: MultiPoly,
+        w_c: MultiPoly,
+    ) -> Self {
+        Self::InterLayer {
+            k,
+            prev_k,
+            add,
+            mult,
+            w_b,
+            w_c,
+        }
+    }
+
+    // Constructor for InputLayer
+    pub fn new_input_layer(k: usize, input_ext: MultiPoly) -> Self {
+        Self::InputLayer {
+            k,
+            input_ext,
+        }
+    }
     pub fn evaluation_ext(&self) -> MultiPoly {
         match self {
             Self::InputLayer { k: _, input_ext } => input_ext.clone(),
@@ -111,9 +157,11 @@ impl Layer {
                 ..
             } => {
                 //let f = mult_poly(add, &(w_b + w_c)) + mult_poly(mult, &mult_poly(&w_b, &w_c));
-                let right1: MLPoly = (w_b + w_c).into();
-                let right2: MLPoly = MLPoly(w_b.clone()) * MLPoly(w_c.clone());
-                let f = (MLPoly(add.clone()) + right1) + (MLPoly(mult.clone()) * right2);
+                let add_poly = MLPoly(add.clone());
+                let right_poly1: MLPoly = (w_b + w_c).into();
+                let mult_poly = MLPoly(mult.clone());
+                let right_poly2: MLPoly = MLPoly(w_b.clone()) * MLPoly(w_c.clone());
+                let f = (add_poly + right_poly1) + (mult_poly * right_poly2);
                 //sum_last_k_var(&f, 2 * prev_k)
                 f.sum_last_k_var(2 * prev_k)
             }
@@ -142,3 +190,102 @@ impl Layer {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use ark_poly::DenseMVPolynomial;
+   // use ark_bn254::Fr as ScalarField;
+   // use ark_ff::One;
+   // use ark_poly::polynomial::multivariate::SparseTerm;
+   
+
+    fn mock_multipoly() -> MultiPoly {
+        MultiPoly::from_coefficients_vec(4, vec![])
+    }
+
+    #[test]
+    fn test_output_layer_initialization() {
+        let add = mock_multipoly();
+        let mult = mock_multipoly();
+        let w_b = mock_multipoly();
+        let w_c = mock_multipoly();
+        let d = mock_multipoly();
+        
+        let output_layer = Layer::new_output_layer(10, 5, add, mult, w_b, w_c, d);
+       
+        match output_layer {
+            Layer::OutputLayer { k, prev_k, add: _, mult: _, w_b: _, w_c: _, d: _ } => {
+                assert_eq!(k, 10);
+                assert_eq!(prev_k, 5);
+            },
+            _ => panic!("Layer was not correctly initialized as OutputLayer"),
+        }
+    }
+
+    #[test]
+    fn test_inter_layer_initialization() {
+        let add = mock_multipoly();
+        let mult = mock_multipoly();
+        let w_b = mock_multipoly();
+        let w_c = mock_multipoly();
+
+        let inter_layer = Layer::new_inter_layer(8, mock_multipoly());
+
+        match inter_layer {
+            Layer::InterLayer { k, prev_k, add: _, mult: _, w_b: _, w_c: _ } => {
+                assert_eq!(k, 8);
+                assert_eq!(prev_k, 4);
+            },
+            _ => panic!("Layer was not correctly initialized as InterLayer"),
+        }
+    }
+
+    #[test]
+    fn test_input_layer_initialization() {
+        let add = mock_multipoly();
+        let mult = mock_multipoly();
+        let w_b = mock_multipoly();
+        let w_c = mock_multipoly();
+        let prev_k = 3;
+
+        let input_layer = Layer::new_input_layer(6, mock_multipoly());
+
+        match input_layer {
+            Layer::InputLayer { k,  ..} => {
+                assert_eq!(k, 6);
+                assert_eq!(prev_k, 3);
+            },
+            _ => panic!("Layer was not correctly initialized as InputLayer"),
+        }
+    }
+
+    #[test]
+    fn test_layer_equality() {
+        let add = mock_multipoly();
+        let mult = mock_multipoly();
+        let w_b = mock_multipoly();
+        let w_c = mock_multipoly();
+
+        let layer1 = Layer::InterLayer {
+            k: 7,
+            prev_k: 3,
+            add: add.clone(),
+            mult: mult.clone(),
+            w_b: w_b.clone(),
+            w_c: w_c.clone(),
+        };
+
+        let layer2 = Layer::InterLayer {
+            k: 7,
+            prev_k: 3,
+            add,
+            mult,
+            w_b,
+            w_c,
+        };
+
+        assert_eq!(layer1, layer2);
+    }
+}
+
